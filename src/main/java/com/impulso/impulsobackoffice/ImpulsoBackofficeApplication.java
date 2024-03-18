80caa43b540e76d47a4581c8b1348ab69eafb63c
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2024-03-18 15:22:38 -0500

    chore: :art: add profiles

---
+++ src/main/java/com/impulso/impulsobackoffice/ImpulsoBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.impulso.impulsobackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulsoBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulsoBackofficeApplication.class, args);
+	}
+
+}
